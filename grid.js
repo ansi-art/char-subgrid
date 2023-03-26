152190fb65aabb14d4468f7531e0d10f43e57050
Abbey Sparrow <abbey@khrome.net>
2023-03-25 20:05:44 -0600

    UMD support on root include

--- grid.js
+++ grid.js
@@ -1,18 +1,41 @@
-const Blocks = require('./src/blocks');
-const Braille = require('./src/braille');
-const Slants = require('./src/slants');
-
-const blocks = new Blocks();
-const braille = new Braille();
-const slants = new Slants();
-module.exports = {
-    blocks : (str)=>{
-        return blocks.transform(str);
-    },
-    braille : (str)=>{
-        return braille.transform(str);
-    },
-    slants : (str)=>{
-        return slants.transform(str);
-    },
-};
+(function (root, factory){
+    if(typeof define === 'function' && define.amd){
+        // AMD. Register as an anonymous module.
+        define([
+            './src/blocks', 
+            './src/braille', 
+            './src/slants'
+        ], factory);
+    }else if (typeof module === 'object' && module.exports){
+        module.exports = factory(
+            require('./src/blocks'), 
+            require('./src/braille'), 
+            require('./src/slants')
+        );
+    }else{
+        // Browser globals (root is window)
+        if(!root.Ascii) root.Ascii = {};
+        if(!root.Ascii.Char) root.Ascii.Char = {};
+        root.Ascii.Char.Grid = factory(
+            root.Ascii.Char.BlocksTransformer, 
+            root.Ascii.Char.BrailleTransformer,
+            root.Ascii.Char.SlantsTransformer
+        );
+    }
+}(this, function(Blocks, Braille, Slants){
+    const blocks = new Blocks();
+    const braille = new Braille();
+    const slants = new Slants();
+    return {
+        Blocks, Braille, Slants,
+        blocks : (str)=>{
+            return blocks.transform(str);
+        },
+        braille : (str)=>{
+            return braille.transform(str);
+        },
+        slants : (str)=>{
+            return slants.transform(str);
+        },
+    };
+}));
